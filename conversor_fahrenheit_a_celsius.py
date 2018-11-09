53449c02ef77cd204ac10d011b5dc2c20f857428
Igoracademy <igor.toro85@gmail.com>
2018-11-09 16:18:09 +0100

    Añadiendo conversor Fahrenheit a Celsius

---
+++ conversor_fahrenheit_a_celsius.py
@@ -0,0 +1,5 @@
+temperatura_fahrenheit = int(input("Inserta el valor de la temperatura en ºFahrenheit: "))
+
+temperatura_celsius = (temperatura_fahrenheit - 32) / 1.8
+
+print("{}ºFahrenheit son: {}ºCelsius".format(temperatura_fahrenheit, temperatura_celsius))
